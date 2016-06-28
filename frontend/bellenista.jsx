f9c8c79a26cae6242bf848b1e4a384f946bfdd40
Jie Wei <email2jie@gmail.com>
2016-06-28 10:04:42 -0700

    Added Setup Files

---
+++ frontend/bellenista.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactRouter = require('react-router');
+const Router = ReactRouter.Router;
+const Route = ReactRouter.Route;
+const IndexRoute = ReactRouter.IndexRoute;
+const HashHistory = ReactRouter.hashHistory;
+
+
+document.addEventListener("DOMContentLoaded", function () {
+ReactDOM.render(<div>Bellenista</div>,
+    document.getElementById('content')
+)});
